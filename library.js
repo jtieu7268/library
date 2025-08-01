94a620d8c87dfb87f4cad751f41e8c0cf2dcc503
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-08-01 19:39:10 -0400

    Implement rudimentary function to display library onto webpage

--- library.js
+++ library.js
@@ -11,3 +11,24 @@ function addBookToLibrary(title, author, pages) {
     const book = new Book(title, author, pages);
     myLibrary.push(book);
 }
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295);
+
+function displayLibrary() {
+    const libraryElement = document.querySelector(".library");
+    for (const book of myLibrary) {
+        const bookCardElement = document.createElement("div");
+        const titleElement = document.createElement("h1");
+        titleElement.textContent = book.title;
+        const authorElement = document.createElement("h2");
+        authorElement.textContent = book.author;
+        const pagesElement = document.createElement("p");
+        pagesElement.textContent = book.pages;
+        bookCardElement.appendChild(titleElement);
+        bookCardElement.appendChild(authorElement);
+        bookCardElement.appendChild(pagesElement);
+        libraryElement.appendChild(bookCardElement);
+    }
+}
+
+displayLibrary()
